a86b3a886fd5aaa2725aa84282f0250057725008
denis-sopizhenko <denchikyc@gmail.com>
2026-05-30 21:34:22 +0300

    Update SensorArray.cs v5

--- SensorArray.cs
+++ SensorArray.cs
@@ -4,24 +4,13 @@ public class SensorArray
 {
     private int _simulatedDistance;
 
-    // Конструктор без параметрів (збережений з попередніх версій)
-    public SensorArray()
-    {
-        _simulatedDistance = 150;
-    }
+    public SensorArray() => _simulatedDistance = 150;
+    public SensorArray(SensorArray other) => this._simulatedDistance = other._simulatedDistance;
 
-    // ВІДПОВІДЬ НА ПОМИЛКУ: Додано конструктор копії, який очікував SmartCar
-    public SensorArray(SensorArray other)
-    {
-        this._simulatedDistance = other._simulatedDistance;
-    }
-
-    // Збережені функції керування дистанцією
     public void UpdateObstacleDistance(int meters) => _simulatedDistance = meters;
     public int GetObstacleDistance() => _simulatedDistance;
     public bool IsPathClear() => _simulatedDistance >= 15;
 
-    // ВЕРСІЯ 4: Унарний оператор '-' (імітація зашумлення)
     public static SensorArray operator -(SensorArray s)
     {
         s._simulatedDistance /= 2;
