70a7b17273be08e46b4b94a8332038c50731ccbd
denis-sopizhenko <denchikyc@gmail.com>
2026-05-30 21:59:24 +0300

    v6
    Version 6 addition

---
+++ CustomExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Виняток: Спроба почати рух із непристебнутими пасажирами
+public class UnsecuredPassengerException : Exception
+{
+    public string PassengerName { get; private set; }
+
+    public UnsecuredPassengerException(string name, string message) : base(message)
+    {
+        PassengerName = name;
+    }
+}
+
+// Виняток: Критичний перегрів або апаратне пошкодження двигуна
+public class EngineOverheatException : Exception
+{
+    public int CurrentTemperature { get; private set; }
+
+    public EngineOverheatException(int temp, string message) : base(message)
+    {
+        CurrentTemperature = temp;
+    }
+}
