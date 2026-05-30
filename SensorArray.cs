1e2ba2aaab912786ede757da26d96d1655e287a6
denis-sopizhenko <denchikyc@gmail.com>
2026-05-30 20:15:30 +0300

    Update SensorArray.cs v2
    Додано статичний конструктор: імітує калібрування лазерних датчиків при першому зверненні до класу.
    
    Додано конструктор копії: копіює налаштування та конфігурацію розташування датчиків.
    
    Приховано поле відстані: змінна SimulatedDistance стала приватною властивістю.

--- SensorArray.cs
+++ SensorArray.cs
@@ -1,10 +1,28 @@
-﻿using System;
+using System;
 
 public class SensorArray
 {
+    // Проста закрита властивість для внутрішніх тестів
+    private int SimulatedDistance { get; set; }
+
+    static SensorArray() => Console.WriteLine("[Static] Proximity sensor array calibrated.");
+
+    public SensorArray()
+    {
+        SimulatedDistance = 150; // Стандартна безпечна відстань (можна змінити для тесту гальм)
+        Console.WriteLine("[Constructor] SensorArray modules deployed.");
+    }
+
+    public SensorArray(SensorArray other)
+    {
+        this.SimulatedDistance = other.SimulatedDistance;
+        Console.WriteLine("[Copy Constructor] Sensor layout replicated.");
+    }
+
+    // Збережена функція повернення дистанції
     public int GetObstacleDistance()
     {
-        // Емуляція сенсорів: повертає безпечну відстань
-        return 120;
+        // Для демонстрації екстреного гальмування змініть повернене значення на < 10
+        return SimulatedDistance;
     }
 }
